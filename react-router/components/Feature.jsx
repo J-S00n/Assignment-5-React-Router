0c3507a973c5f8031c03594b49c163f91fef8546
J-S00n <jason.chen20@student.tdsb.on.ca>
2025-04-26 22:57:54 -0400

    Added feature section in HomeView

--- react-router/components/Feature.jsx
+++ react-router/components/Feature.jsx
@@ -2,10 +2,69 @@ import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 
 function Feature() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const page = useRef(1);
+    const pages = useRef(0);
+
+    const getMoreMovies = async (direction) => {
+        const nextPage = page.current + direction;
+        if (nextPage > 0 && nextPage <= pages.current) {
+            setLoading(true);
+            page.current = nextPage;
+            try {
+                const response = await axios.get(
+                    `https://api.themoviedb.org/3/movie/now_playing?page=${nextPage}&api_key=${import.meta.env.VITE_TMDB_KEY}`
+                );
+                setMovies(response.data.results);
+                pages.current = response.data.total_pages;
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+    };
+
+    useEffect(() => {
+        const getMovies = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`);
+                pages.current = response.data.total_pages;
+                setMovies(Array.isArray(response.data.results) ? response.data.results : []);
+
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getMovies();
+    }, []);
+
 
     return (
-        <div className="feature">
-            <h2>Feature Component</h2>
+        <div className="feature-container">
+            <h1>Featured Movies</h1>
+            <div className="movie-list">
+                {movies.length === 0 ? (
+                    <p>Loading...</p>
+                ) : (
+                    movies.slice(0, 3).map((movie) => (
+                        <div key={movie.id} className="movie-card">
+                            <h1>{`${movie.title}`}</h1>
+                            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.id}`} />
+                        </div>
+                    ))
+                )}
+                <div className="pagination">
+                    <button className="previous-button" onClick={() => getMoreMovies(-1)} disabled={loading}>Previous</button>
+                    <button className="next-button"onClick={() => getMoreMovies(1)} disabled={loading}>Next</button>
+                </div>
+                {loading && <p>Loading...</p>}
+            </div>
         </div>
     );
 }
